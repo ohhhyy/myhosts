27dfb8aaf666eac37337b34b5f825054dfa8a39f
ohhhyy <48433551+ohhhyy@users.noreply.github.com>
2020-11-07 19:54:04 +0800

    Add files via upload

--- node.sh
+++ node.sh
@@ -1,16 +1,24 @@
 #! /bin/bash
 clear
 read -p '欢迎使用玩客云armbian 甜糖CDN自动部署程序
-本程序全自动判断系统版本，不管是N1还是玩客云都可以一键部署！                                
+本程序全自动判断系统版本，不管是N1还是玩客云都可以一键部署！
+
+#######  #####  #######  #####   #####   #####  
+#       #     # #    #  #     # #     # #     # 
+#       #     #     #   #     # #     # #     # 
+######   #####     #     #####   #####   #####  
+      # #     #   #     #     # #     # #     # 
+#     # #     #   #     #     # #     # #     # 
+ #####   #####    #      #####   #####   #####                                                
 
 ==============================================
-    填写邀请码：428050  免费获取15张加成卡！
+    填写邀请码：587888  免费获取15张加成卡！
 ==============================================
 玩客云刷armbian视频教程：https://post.smzdm.com/p/awx4rqkk/
 
-请输入邀请码 428050 开始自动部署：' number
+请输入邀请码 587888 开始自动部署：' number
  
-if [ $number = 428050 ];then
+if [ $number = 587888 ];then
 	echo "输入正确，开始部署！"         
 	cp -pdr /etc/rc.local /etc/rc.local.default
 	cp -pdr /etc/crontab /etc/crontab.default
@@ -27,19 +35,19 @@ if [ $number = 428050 ];then
 	if [ $(getconf WORD_BIT) = '32' ];then
 		echo "32位系统，3秒后即将开始部署32位甜糖程序!"
 		sleep 3s
-		wget http://www.ohhhyy.fun:8888/down/H3P6FdmrPxU5 -O ttnode
+		wget https://dachui.co/ttnode/ttnode32 -O ttnode
 	else
 		echo "64位系统，3秒后即将开始部署64位甜糖程序!"
 		sleep 3s
-		wget http://www.ohhhyy.fun:8888/down/CHjGX2VzhsrP -O ttnode
+		wget https://dachui.co/ttnode/ttnode64 -O ttnode
 	fi
-		wget http://www.ohhhyy.fun:8888/down/ns2vjiEAl70I
-		wget http://www.ohhhyy.fun:8888/down/RIb9pBVKBPAU
+		wget https://dachui.co/ttnode/crash_monitor.sh
+		wget https://dachui.co/ttnode/log.log
 	chmod -R 777 *
 	
 	#cd /etc
 	#rm -rf rc.local
-	#wget http://www.ohhhyy.fun:8888/down/GtgfrcwcftOW
+	#wget http://94.191.86.51/node/rc.local
 	#chmod -R 777 rc.local
 	
 	sed -i "12a mount $fenqu /mnts/\nservice sshd start\n/usr/node/ttnode -p /mnts" /etc/rc.local
@@ -59,17 +67,17 @@ if [ $number = 428050 ];then
 	
 	===================================================
 	===================================================
-	      请大力填写我的甜糖发财邀请码：428050
+	      请大力填写我的甜糖发财邀请码：587888
 	===================================================
 	===================================================
 	
 	此脚本由「折了个腾」原创发布，没有shell开发经验，现学现卖写的
 	
-	开发不易，新来的朋友填我的推荐码 428050 支持一下，感谢！
+	开发不易，新来的朋友填我的推荐码 587888 支持一下，感谢！
 	
 	--------------------------------------------------------------------------------------------------
 	"
 
 else
-	echo "输入有误！我的邀请码是：428050"
+	echo "输入有误！我的邀请码是：587888"
 fi
